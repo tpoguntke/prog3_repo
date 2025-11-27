eb0be37e6d8fd962f222325750513d266df160a9
Tim Poguntke <tim.poguntke@hs-kempten.de>
2025-11-27 14:45:22 +0100

    complete rounting2

--- routing2.py
+++ routing2.py
@@ -1,12 +1,20 @@
 from openrouteservice import client
 import folium
 import webbrowser
+import yaml
+import argparse
 
-def make_map():
-    map_kempten = folium.Map(location=([47.72167778891493, 10.313657870248395]), zoom_start=15)  # Create map
+def make_map(config):
 
-    folium.Marker([47.716080, 10.313525], popup='Hochschule Kreisel').add_to(map_kempten)
-    folium.Marker([47.728723272725006, 10.312297325822115], popup='Residenz').add_to(map_kempten)
+    center = config.get('map_center', [47.32167778891493, 10.313657870248395])
+    zoom_start = config.get('zoom_start', 15)
+    map_kempten = folium.Map(location=center, zoom_start=zoom_start)  # Create map
+
+    for marker in config.get('markers', []):
+        lat = marker.get('lat')
+        lon = marker.get('lon')
+        popup = marker.get('popup', '') 
+        folium.Marker([lat, lon], popup=popup).add_to(map_kempten)
 
     return map_kempten
 
@@ -16,17 +24,30 @@ def style_function(color):  # To style data
                                 opacity=0.5,
                                 weight=4, )
 
-def add_route(map):
-    api_key = '5b3ce3597851110001cf6248ceb027611ac04ba796da742ff02ea8d1'  # https://openrouteservice.org/sign-up
+def add_route(map, config):
+
+
+    api_key = config.get('api_key', '')  # https://openrouteservice.org/sign-up
+    
     ors = client.Client(key=api_key)
 
+    route_config = config.get('route', {})
+
+    coordinates = route_config.get('coordinates', [])
+
     # Request route
-    coordinates = [[10.313525, 47.716080], [10.312297325822115, 47.728723272725006]]
+    coordinates = [coordinates[0], coordinates[1]]
+
+    preference = route_config.get('preference', 'shortest')
+    format_out = route_config.get('format_out', 'geojson')
+    profile = route_config.get('profile', 'driving-car')
+    geometry = route_config.get('geometry', 'true')
+
     direction_params = {'coordinates': coordinates,
-                    'profile': 'driving-car',  # 'driving-car', 'cycling-regular', 'foot-walking'
-                    'format_out': 'geojson',
-                    'preference': 'shortest',  # 'fastest', 'shortest'
-                    'geometry': 'true'}
+                    'profile': profile,  # 'driving-car', 'cycling-regular', 'foot-walking'
+                    'format_out': format_out,
+                    'preference': preference,  # 'fastest', 'shortest'
+                    'geometry': geometry}
 
     regular_route = ors.directions(**direction_params)
 
@@ -36,10 +57,10 @@ def add_route(map):
     .add_to(map)
     return
 
-def main():
+def main(config):
 
-    m = make_map()
-    add_route(m)
+    m = make_map(config)
+    add_route(m, config)
 
     m.save("index.html")
     webbrowser.open("index.html")
@@ -48,5 +69,13 @@ def main():
 
 
 if __name__ == "__main__":
-    print('Ich werde ausgeführt!')
-    main()
+    parser = argparse.ArgumentParser(description='Routing Application')
+    parser.add_argument('--config', type=str, default='config.yaml',
+                        help='Path to the configuration YAML file')
+    args = parser.parse_args()
+
+    cfg = yaml.safe_load(open(args.config))
+
+    print(cfg)
+
+    main(cfg)
